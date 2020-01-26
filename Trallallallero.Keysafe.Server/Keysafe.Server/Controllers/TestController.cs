de31d6926228a4527ec7f70798a4b0cb519c5c5d
Trallallallero <trallallallero@trallallallero.it>
2020-01-26 17:46:47 +0100

    test controller

---
+++ Trallallallero.Keysafe.Server/Keysafe.Server/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Keysafe.Server.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class TestController : ControllerBase
+	{
+		#region Public Methods
+
+		[HttpGet]
+		public IActionResult TestMethod()
+		{
+			return this.Ok("Test method called");
+		}
+
+		#endregion Public Methods
+	}
+}
--- Trallallallero.Keysafe.Server/Keysafe.Server/Startup.cs
+++ Trallallallero.Keysafe.Server/Keysafe.Server/Startup.cs
@@ -1,32 +1,29 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Keysafe.Server
 {
 	public class Startup
 	{
-		public Startup(IConfiguration configuration)
-		{
-			Configuration = configuration;
-		}
+		#region Public Properties
 
 		public IConfiguration Configuration { get; }
 
-		// This method gets called by the runtime. Use this method to add services to the container.
-		public void ConfigureServices(IServiceCollection services)
+		#endregion Public Properties
+
+		#region Public Constructors
+
+		public Startup(IConfiguration configuration)
 		{
-			services.AddControllers();
+			this.Configuration = configuration;
 		}
+
+		#endregion Public Constructors
+
+		#region Public Methods
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -47,5 +44,13 @@ namespace Keysafe.Server
 				endpoints.MapControllers();
 			});
 		}
+
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddControllers();
+		}
+
+		#endregion Public Methods
 	}
 }
